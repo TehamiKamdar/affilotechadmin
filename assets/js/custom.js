23a27e784101ec9269f36631dffafd7a965f1c7d
TehamiKamdar <tehamikamdar15@gmail.com>
2025-07-15 11:36:18 +0500

    pages added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 /**
  *
- * You can write your JS code here, DO NOT touch the default style file
+ * You can write your JS code here, DO NOT touch the default javascript file
  * because it will make it harder for you to update.
  * 
  */
